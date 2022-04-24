34ac6d8086f0e2137abf95d04b9cfe5d05357341
SmollestFroggy <91900231+SmollestFroggy@users.noreply.github.com>
2022-04-24 13:20:53 -0400

    Create RuneFormula.java

---
+++ GitEldenRing/src/main/java/com/mycompany/giteldenring/RuneFormula.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.eldenringcalculator;
+
+/**
+ *
+ * @author chef
+ */
+public class RuneFormula {
+    /**
+     * Hero = 7
+     * bandit =5 
+     * astro =6 
+     * warrior =8 
+     * prisoner = 9 
+     * confessor = 10
+     * wretch = 1
+     * vagabond = 9
+     * prophet = 7
+     * samurai = 9 
+     */
+    int base_RuneAmountToGetToLevelTwelve;
+    int total_levels= 12;
+    int x = total_levels;
+    int total_runes = 0;
+    int y = total_runes;
+   public int car
+   if base_level = 7 
+           base_RuneAmountToGetToLevelTwelve = 775 + 793 + 811 + 829; 
+           else if 
+           
+           else if 
+           else if
+           
+   total_runes = ((0.02x^3 + 3.06x^2 + 105.6x - 895)+ base_RuneAmountToGetToLevelTwelve)
+           
+}
